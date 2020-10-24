c75238315e9e31448349f48abc50d560d8f15167
rosullivan7 <rourke.osullivan7@gmail.com>
2020-10-24 16:13:18 +1100

    router & nav links
    added react router dom with router and nav links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from "react-bootstrap/Image";
 import banner from './components/images/bannerImg.JPG';
+import About from './components/pages/About';
 
 
 import './App.css';
@@ -10,10 +12,17 @@ import './App.css';
 class App extends Component{
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Image src={ banner } fluid />
-      </div>
+      <Router>
+        <div className="App">
+            <NavBar />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <Image src={ banner } fluid />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+        </div>
+      </Router>
     );
     }
 }
